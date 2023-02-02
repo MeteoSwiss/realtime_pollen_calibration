283b8bec3be6a35240242163a96cc68a9dd11fea
Simon Adamov <simon.adamov@mailbox.org>
2023-02-02 11:45:10 +0100

    Fixed environments files Update to latest copier

--- src/realtime_pollen_calibration/main.py
+++ src/realtime_pollen_calibration/main.py
@@ -1,25 +1,27 @@
 """Run test cases for realtime calibration."""
 
 # Standard library
+from pathlib import Path
 import os
 
 # First-party
 from realtime_pollen_calibration import update_phenology_realtime
 from realtime_pollen_calibration import update_strength_realtime
 
-cwd = os.getcwd()
+cwd = Path(os.getcwd())
+data_path = str(cwd.parent.parent.absolute())
 
-file_data = cwd + "/data/atabs/alnu_pollen_measured_values_2022021314.atab"
-file_grib = cwd + "/data/grib2_files_cosmo1e/laf2022021314_ALNUtthrs_tthre"
-file_out = cwd + "/data/2022020815_ALNUtthrs_tthre"
+file_data = data_path + "/data/atabs/alnu_pollen_measured_values_2022021314.atab"
+file_grib = data_path + "/data/grib2_files_cosmo1e/laf2022021314_ALNUtthrs_tthre"
+file_out = data_path + "/data/2022020815_ALNUtthrs_tthre"
 update_phenology_realtime.update_phenology_realtime(
     file_data, file_grib, file_out, False
 )
 
-file_data = cwd + "/data/atabs/alnu_pollen_measured_values_2022022207.atab"
-file_data_mod = cwd + "/data/atabs/alnu_pollen_modelled_values_2022022207.atab"
-file_grib = cwd + "/data/grib2_files_cosmo1e/laf2022022207_ALNUtune"
-file_out = cwd + "/data/2022022208_ALNUtune"
+file_data = data_path + "/data/atabs/alnu_pollen_measured_values_2022022207.atab"
+file_data_mod = data_path + "/data/atabs/alnu_pollen_modelled_values_2022022207.atab"
+file_grib = data_path + "/data/grib2_files_cosmo1e/laf2022022207_ALNUtune"
+file_out = data_path + "/data/2022022208_ALNUtune"
 update_strength_realtime.update_strength_realtime(
     file_data, file_data_mod, file_grib, file_out, False
 )
