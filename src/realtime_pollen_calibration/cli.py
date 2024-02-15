72dd88411b348606a0ef659adfe90957b397afea
Tobias Wicky <tobias.wicky@meteoswiss.ch>
2024-02-15 15:45:08 +0100

    pop some stashes

--- src/realtime_pollen_calibration/cli.py
+++ src/realtime_pollen_calibration/cli.py
@@ -1,16 +1,11 @@
 """Command line interface of realtime_pollen_calibration."""
-# Standard library
-import logging
 
 # Third-party
 import click
 
-# First-party
-from realtime_pollen_calibration.utils import count_to_log_level
 
 # Local
 from . import __version__
-from .mutable_number import MutableNumber
 
 
 # pylint: disable-next=W0613  # unused-argument (param)
@@ -21,39 +16,6 @@ def print_version(ctx, param, value: bool) -> None:
         ctx.exit(0)
 
 
-# pylint: disable-next=W0613  # unused-argument (args, kwargs)
-def set_log_level(ctx, param, value: int) -> int:
-    """Set the logging level."""
-    logging.getLogger().setLevel(count_to_log_level(value))
-    return value
-
-
-@click.pass_context
-# pylint: disable-next=W0613  # unused-argument (args, kwargs)
-def print_number(ctx, *args, **kwargs) -> None:
-    """Print the current number."""
-    number = ctx.obj["number"].get()
-    click.echo(f"{number:g}")
-
-
-def print_operation(ctx, operator: str, value: float) -> None:
-    if ctx.obj["verbosity"]:
-        number = ctx.obj["number"]
-        click.echo(f"{number.get(-2):g} {operator} {value:g} = {number.get():g}")
-
-
-@click.group(
-    context_settings={"help_option_names": ["-h", "--help"]},
-    no_args_is_help=True,
-    invoke_without_command=True,
-    chain=True,
-    result_callback=print_number,
-)
-@click.argument(
-    "number",
-    type=float,
-    nargs=1,
-)
 @click.option(
     "--version",
     "-V",
@@ -62,50 +24,20 @@ def print_operation(ctx, operator: str, value: float) -> None:
     expose_value=False,
     callback=print_version,
 )
-@click.option(
-    "--verbose",
-    "-v",
-    "verbosity",
-    help="Increase verbosity (specify multiple times for more).",
-    count=True,
-    callback=set_log_level,
-)
-@click.pass_context
-def main(ctx, number: float, **kwargs) -> None:
-    """Console script for test_cli_project."""
-    if ctx.obj is None:
-        ctx.obj = {}
-    ctx.obj["number"] = MutableNumber(number)
-    ctx.obj.update(kwargs)
+@click.group()
+def main():
+    pass
 
 
-@main.command("plus", help="addition")
-@click.argument("addend", type=float, nargs=1)
-@click.pass_context
-def plus(ctx, addend: float) -> None:
-    ctx.obj["number"].add(addend)
-    print_operation(ctx, "+", addend)
+@main.command("update_strength")
+@click.argument("data_path", type=click.Path(exists=True, readable=True, dir_okay=True))
+@click.argument("date_string", type=str)
+def update_strength(data_path: str, date_string: str):
+    pass
 
 
-@main.command("minus", help="subtraction")
-@click.argument("subtrahend", type=float, nargs=1)
-@click.pass_context
-def minus(ctx, subtrahend: float) -> None:
-    ctx.obj["number"].subtract(subtrahend)
-    print_operation(ctx, "-", subtrahend)
-
-
-@main.command("times", help="multiplication")
-@click.argument("factor", type=float, nargs=1)
-@click.pass_context
-def times(ctx, factor: float) -> None:
-    ctx.obj["number"].multiply(factor)
-    print_operation(ctx, "*", factor)
-
-
-@main.command("by", help="division")
-@click.argument("divisor", type=float, nargs=1)
-@click.pass_context
-def by(ctx, divisor: float) -> None:
-    ctx.obj["number"].divide(divisor)
-    print_operation(ctx, "/", divisor)
+@main.command("update_phenology")
+@click.argument("data_path", type=click.Path(exists=True, readable=True, dir_okay=True))
+@click.argument("date_string", type=str)
+def update_phenology():
+    pass
--- src/realtime_pollen_calibration/main.py
+++ src/realtime_pollen_calibration/main.py
@@ -14,23 +14,41 @@ from pathlib import Path
 from realtime_pollen_calibration import update_phenology_realtime
 from realtime_pollen_calibration import update_strength_realtime
 
-cwd = Path(os.getcwd())
-data_path = str(cwd)
 
-file_obs_stns = data_path + "/data/atabs/cory_alnu_pollen_measured_values_2024020118.atab"
-file_mod_stns = data_path + "/data/atabs/cory_alnu_pollen_modelled_values_2024020118.atab"
-file_POV_in   = data_path + "/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_2024020118_update_phen_tune4"
-file_T_2M     = data_path + "/data/grib2_files_ICON-CH1/iaf2024013000"
-file_Const    = data_path + "/data/grib2_files_ICON-CH1/lfff00000000c"
-file_POV_tmp  = data_path + "/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_2024020118_update_phen_tune4_tmp"
-file_POV_out  = data_path + "/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_2024020118_update_phen_tune5"
+def main(data_path: str, date: str):
+    file_obs_stns = (
+        data_path + f"/data/atabs/cory_alnu_pollen_measured_values_{date}.atab"
+    )
+    file_mod_stns = (
+        data_path + f"/data/atabs/cory_alnu_pollen_modelled_values_{date}.atab"
+    )
+    file_POV_in = (
+        data_path
+        + f"/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_{date}_update_phen_tune4"
+    )
+    file_T_2M = data_path + f"/data/grib2_files_ICON-CH1/iaf{date}"
+    file_Const = data_path + f"/data/grib2_files_ICON-CH1/lfff00000000c"
+    file_POV_tmp = (
+        data_path
+        + f"/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_{date}_update_phen_tune4_tmp"
+    )
+    file_POV_out = (
+        data_path
+        + f"/data/grib2_files_ICON-CH1/ART_POV_iconR19B08-grid_0001_{date}_update_phen_tune5"
+    )
 
-hour_incr  = 0
-update_phenology_realtime.update_phenology_realtime(
-    file_obs_stns, file_POV_in, file_T_2M, file_Const, file_POV_tmp, hour_incr, True
-)
+    hour_incr = 0
+    update_phenology_realtime.update_phenology_realtime(
+        file_obs_stns, file_POV_in, file_T_2M, file_Const, file_POV_tmp, hour_incr, True
+    )
 
-hour_incr  = 1
-update_strength_realtime.update_strength_realtime(
-    file_obs_stns, file_mod_stns, file_POV_tmp, file_Const, file_POV_out, hour_incr, True 
-)
+    hour_incr = 1
+    update_strength_realtime.update_strength_realtime(
+        file_obs_stns,
+        file_mod_stns,
+        file_POV_tmp,
+        file_Const,
+        file_POV_out,
+        hour_incr,
+        True,
+    )
