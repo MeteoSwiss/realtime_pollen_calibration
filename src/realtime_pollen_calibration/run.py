4834117d8db1cd00055c22841e532e3a6544b58e
Tobias Wicky <tobias.wicky@meteoswiss.ch>
2024-02-27 15:48:04 +0100

    add run file

---
+++ src/realtime_pollen_calibration/run.py
@@ -0,0 +1,10 @@
+"""Entry point for module call."""
+# Standard library
+import gc
+
+# First-party
+from realtime_pollen_calibration.cli import main
+
+if __name__ == "__main__":
+    main()
+    gc.collect()
