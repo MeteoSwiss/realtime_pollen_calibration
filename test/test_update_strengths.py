ea170aa848427bcbe746ecaad9025bfdd4a5cfbe
Andreas Pauling <paa@balfrin-ln002.cscs.ch>
2025-11-26 17:43:55 +0100

    cleaning and updating the README

--- test/test_update_strengths.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from realtime_pollen_calibration.update_strength import update_strength_realtime
-
-def test_update_strength_realtime(caplog, config):
-
-    _, parsed_config = config
-    
-    update_strength_realtime(parsed_config)
